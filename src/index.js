30743f02a595f30f8fba738806d561d508cf037a
AleksTret <Aleks.Tret@gmail.com>
2020-10-01 14:47:26 +0300

    feat: add min(), max(), avg()

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 
 exports.min = function min (array) {
-  return 0;
+  return array ? array.sort((prev, next) => prev - next)[0] || 0 : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return array ? array.sort((prev, next) => next - prev)[0] || 0 : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	return array ? array.reduce((sum, item) => sum + (+item), 0) / array.length || 0 : 0;
 }
+
